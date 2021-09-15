eaa473044ff6b5aa71017e4a99e790a9e42254ee
romlopez1 <romibernard68@gmail.com>
2021-09-15 09:08:57 -0500

    se agregan revisiones de clase

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // ...
 
 // Add the route handlers here:
+// /beers
+app.get("/beers", (req, res) => {
+  res.render("beers")
+})
 
-app.get('/', (req, res) => {
+// /random-beers
+app.get("/random-beer", (req, res) => {
+  res.render("random-beer")
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', { beers: responseFromAPI });
+  })
+  .catch(error => console.log(error));
+
+})
+
+// /home
+app.get("/", (req, res) => {
   res.render('index');
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
